181cf23d8d89df00d1fefc9bc56386e1f78285dc
Robert Israelsson <israelsson.r@gmail.com>
2021-10-01 12:29:04 +0200

    Added JWT_SECRET to production env variable.

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 "use strict";
 
+require('dotenv-flow').config();
 var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcryptjs');
@@ -15,7 +16,7 @@ router.post('/', async function(req, res) {
     bcrypt.compare(req.body.password, passwordHash, function(err, result) {
         if (result) {
             // Generate an access token
-            const accessToken = jwt.sign({ username: user.username }, config.jwt_secret);
+            const accessToken = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
             
             // Send generated acess token as response to client.
             return res.json({accessToken});
